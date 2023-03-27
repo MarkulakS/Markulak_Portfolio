589794ef376480eb0ad027d7914c66373bc0ee17
MarkulakS <markulaks17@gmail.com>
2023-03-27 18:17:06 +0200

    Creating all sections. Adding styling to sections.

--- portfolio/src/app/components/main-page/main-page.component.ts
+++ portfolio/src/app/components/main-page/main-page.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { ScrollDispatcher, CdkScrollable  } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit  {
 
-  constructor() { }
+  constructor(private scrollDispatcher: ScrollDispatcher, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+    // const cubeElement = this.elementRef.nativeElement.querySelector('#cube');
+    this.scrollDispatcher.scrolled().subscribe(() => {
+      const scrollPositionStart = 700;
+      const scrollPositionY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      if(scrollPositionY >= scrollPositionStart && scrollPositionY-700 <= 1300) {
+        // cubeElement.style.left = `${scrollPositionY-700 + 5}px`;
+        // console.log("Y: " +scrollPositionY);
+      }      // }else {
+      //   cubeElement.style.rght = `${scrollPositionY + 5}px`;
+      //   // console.log("right Y: " +scrollPositionY);
+      // }
+    });
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if(!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.classList.add('show');
+          observer.unobserve(entry.target);
+        }
+      })
+    })
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
   }
+  
 
 }
--- portfolio/src/app/components/navbar/navbar.component.ts
+++ portfolio/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ScrollDispatcher } from '@angular/cdk/scrolling';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private scrollDispatcher: ScrollDispatcher) { }
 
   ngOnInit(): void {
+    const element = this.elementRef.nativeElement.querySelector('#nav');
+    this.scrollDispatcher.scrolled().subscribe(() => {
+      const scrollPositionStart = 700;
+      const scrollPositionY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      if(scrollPositionY <= 700) {
+        element.style.backgroundColor = `#012E40`;
+        console.log("Y top: " +scrollPositionY);
+      }else if (scrollPositionY > 700 && scrollPositionY < 915) {
+        element.style.backgroundColor = `transparent`;
+        console.log("Y trans: " +scrollPositionY);
+      }else if (scrollPositionY >= 915 && scrollPositionY < 2200) {
+        element.style.backgroundColor = `#131316`;
+        console.log("Y mid: " +scrollPositionY);
+      }else if (scrollPositionY > 2200 && scrollPositionY < 2400 ) {
+        element.style.backgroundColor = `transparent`;
+        console.log("Y trans: " +scrollPositionY);
+      }else if (scrollPositionY >= 2400) {
+        element.style.backgroundColor = `#025159`;
+        console.log("Y bot: " +scrollPositionY);
+      }
+    });
   }
 
 }
