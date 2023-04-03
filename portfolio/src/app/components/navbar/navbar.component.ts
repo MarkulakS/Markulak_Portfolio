baf1a5d3e13a0c1f4dcc7e67f9f997a8ddd86d56
MarkulakS <markulaks17@gmail.com>
2023-04-03 19:29:09 +0200

    Navbar and Footer are responsive

--- portfolio/src/app/components/navbar/navbar.component.ts
+++ portfolio/src/app/components/navbar/navbar.component.ts
@@ -17,21 +17,27 @@ export class NavbarComponent implements OnInit {
       const scrollPositionY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
       if(scrollPositionY <= 700) {
         element.style.backgroundColor = `#012E40`;
-        console.log("Y top: " +scrollPositionY);
       }else if (scrollPositionY > 700 && scrollPositionY < 915) {
         element.style.backgroundColor = `transparent`;
-        console.log("Y trans: " +scrollPositionY);
       }else if (scrollPositionY >= 915 && scrollPositionY < 2200) {
         element.style.backgroundColor = `#131316`;
-        console.log("Y mid: " +scrollPositionY);
       }else if (scrollPositionY > 2200 && scrollPositionY < 2400 ) {
         element.style.backgroundColor = `transparent`;
-        console.log("Y trans: " +scrollPositionY);
       }else if (scrollPositionY >= 2400) {
         element.style.backgroundColor = `#025159`;
-        console.log("Y bot: " +scrollPositionY);
       }
     });
   }
 
+  addBounce() {
+    var element = document.getElementById('icon-message');
+    element?.classList.add("fa-bounce");
+
+    const firstTimeout = setTimeout(() => {
+      element?.classList.remove("fa-bounce");
+    }, 1000);
+  }
+  
+
 }
+
