aa5f860c5e3a76009edeeb35ab444207634c8c6b
MarkulakS <markulaks17@gmail.com>
2023-04-13 20:00:13 +0200

    Created hamburger; Improved responsiveness

--- portfolio/src/app/components/navbar/navbar.component.ts
+++ portfolio/src/app/components/navbar/navbar.component.ts
@@ -11,10 +11,25 @@ export class NavbarComponent implements OnInit {
   constructor(private elementRef: ElementRef, private scrollDispatcher: ScrollDispatcher) { }
 
   ngOnInit(): void {
+    var burger = document.getElementById('hamburger');
+    var list = document.getElementById('list-mobile');
+
+
     const element = this.elementRef.nativeElement.querySelector('#nav');
     this.scrollDispatcher.scrolled().subscribe(() => {
-      const scrollPositionStart = 700;
+
+      // const scrollPositionStart = 700;
       const scrollPositionY = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      let actualPosition = 0;
+      
+      if (burger?.classList.contains("clicked")) {
+        actualPosition = scrollPositionY;
+        if (actualPosition != 0) {
+          burger?.classList.remove("clicked");
+          list?.classList.remove("list-display-on")
+        }
+      }
+
       if(scrollPositionY <= 700) {
         element.style.backgroundColor = `#012E40`;
       }else if (scrollPositionY > 700 && scrollPositionY < 915) {
@@ -46,6 +61,22 @@ export class NavbarComponent implements OnInit {
       element?.classList.remove("fa-bounce");
     }, 1000);
 
+  }
+
+  focusHamburger() {
+    var burger = document.getElementById('hamburger');
+    var list = document.getElementById('list-mobile');
+
+    if (!burger?.classList.contains("clicked")) 
+    {
+      burger?.classList.add("clicked");
+      list?.classList.add("list-display-on")
+    }
+    else if (burger?.classList.contains("clicked")) 
+    {
+      burger?.classList.remove("clicked");
+      list?.classList.remove("list-display-on")
+    }
   }
   
 
